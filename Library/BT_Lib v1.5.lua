33d73699fd360aa1e12e489ec7bbcfde9c9ae996
James <42681518+jmesfo0@users.noreply.github.com>
2023-01-27 23:58:15 -0800

    Update BT_Lib v1.5.lua

--- Library/BT_Lib v1.5.lua
+++ Library/BT_Lib v1.5.lua
@@ -247,7 +247,7 @@ function Library:CreateWindow(title, gameName)
     CoverBT.Position = UDim2.new(0.100220447, 0, 0, 150)
 
     openBT.Name = "openBT"
-    openBT.Parent = CoverBT
+    openBT.Parent = ScreenGui
     openBT.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     openBT.BackgroundTransparency = 1.000
     openBT.BorderSizePixel = 0
