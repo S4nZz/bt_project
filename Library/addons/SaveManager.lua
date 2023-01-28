d0a52d24cc91cab412ec0c3230190a9171899f37
James <42681518+jmesfo0@users.noreply.github.com>
2023-01-27 21:25:09 -0800

    Update SaveManager.lua

--- Library/addons/SaveManager.lua
+++ Library/addons/SaveManager.lua
@@ -47,7 +47,7 @@ local SaveManager = {} do
 				end
 			end,
 		},
-		CreateParagraph = {
+		Paragraph = {
 			Save = function(idx, object)
 				return { type = 'Paragraph', idx = idx, value = object.Value }
 			end,
